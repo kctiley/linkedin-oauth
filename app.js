2d60ac35287999ae480590a64d017b619fe502af
Klint Tiley <kctiley@gmail.com>
2015-10-14 16:12:32 -0600

    No errors after adding dotenv variables and updating config passport

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport')
-
+var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+require('dotenv').load()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,9 +24,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+   done(null, user);
+});
 
 app.use('/', routes);
 app.use('/users', users);
+
+//passport/linked in handling
+passport.use(new LinkedInStrategy({
+  clientID: process.env.LINKEDIN_CLIENT_ID,
+  clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+  callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback",
+  scope: ['r_emailaddress', 'r_basicprofile'],
+  // state: true
+  },  
+  function(accessToken, refreshToken, profile, done) {
+  // asynchronous verification, for effect...
+    done(null, {id: profile.id, displayName: profile.displayName, token: accessToken})
+    }
+));
+
+app.get('/auth/linkedin',
+  passport.authenticate('linkedin', { state: 'SOME_STATE'  }),
+  function(req, res){
+    // The request will be redirected to LinkedIn for authentication, so this
+    // function will not be called.
+  });
+
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,40 +85,6 @@ if (app.get('env') === 'development') {
 }
 
 
-//LINKEDIN PASSPORT OAUTH START***********************************************
-var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
-
-passport.use(new LinkedInStrategy({
-  clientID: '78fwenasx16dg8',
-  clientSecret: 'BNlyK2jicgxs60G4',
-  callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback",
-  scope: ['r_emailaddress', 'r_basicprofile'],
-  state: true
-}, function(accessToken, refreshToken, profile, done) {
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-    // To keep the example simple, the user's LinkedIn profile is returned to
-    // represent the logged-in user. In a typical application, you would want
-    // to associate the LinkedIn account with a user record in your database,
-    // and return that user instead.
-    return done(null, profile);
-  });
-}));
-
-app.get('/auth/linkedin',
-  passport.authenticate('linkedin'),
-  function(req, res){
-    // The request will be redirected to LinkedIn for authentication, so this
-    // function will not be called.
-  });
-
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-  successRedirect: '/',
-  failureRedirect: '/login'
-}));
-//LINKEDIN PASSPORT OAUTH END***********************************************
-
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -92,8 +95,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(4000, function(){
-  console.log("Linkedin-Oauth now listening at Port...4000")
-})
+
 
 module.exports = app;
